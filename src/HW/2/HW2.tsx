3201888f57b1d3a76361011ee9f9ce59cad42b07
Anastasija <anastasijakoles@gmail.com>
2023-10-17 22:13:12 +0300

    feat: update callback on Click for LA button

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -55,7 +55,8 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = users.myFriends.filter((user) => user.address.city === 'Los Angeles')/* 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES'; */
+    debugger
+    const filteredUsers = users.myFriends.filter((user) => user.address.city === 'Los Angeles');/* 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES'; */
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
