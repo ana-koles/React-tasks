530d357b1b8f1e4868e867a84a411cdc8e732d81
Anastasija <anastasijakoles@gmail.com>
2023-10-17 21:59:38 +0300

    feat: add HW2 solution

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { UserList2 } from './UserList2';
 
-export type AddressType = {
+type AddressType = {
   street: string;
   city: string;
 };
@@ -10,11 +10,19 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
+
+type myFriendsType = {
+	id: number
+	name: string
+	age: number
+	address: AddressType
+}
+
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: myFriendsType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -47,13 +55,14 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter((user) => user.address.city === 'Los Angeles')/* 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES'; */
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
   return (
     <div id={'hw02'}>
-      <UserList2 users={currentUsers} filterUsers={filterUsers} />
+      <UserList2 users={currentUsers}
+                  filterUsers={filterUsers} />
     </div>
   );
 };
